fa6e288944e6fb3656944304df58492ba1878f90
Yunus emre salcan <yunusemresalcan@gmail.com>
2023-06-03 23:50:03 +0300

    Update App.js

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -1,24 +1,24 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SingleCard from './components/SingleCard';
 
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png",matched:false },
+  { "src": "/img/potion-1.png",matched:false },
+  { "src": "/img/ring-1.png",matched:false },
+  { "src": "/img/scroll-1.png",matched:false },
+  { "src": "/img/shield-1.png",matched:false },
+  { "src": "/img/sword-1.png",matched:false },
 ]
 
 function App() {
 
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
-  const[choiceOne,setChoiceOne]= useState(null)
-  const[choiceTwo,setChoiceTwo]= useState(null)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   //! Shuffle Cards 
   const shuffleCards = () => {
@@ -29,13 +29,50 @@ function App() {
     setCards(shuffledCards)
     setTurns(0)
   }
-  console.log(cards, turns)
+
 
   //! Handle a Choice 
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
 
+
+  //! Compare 2 Selected cards
+
+  useEffect(() => {
+    if(choiceOne && choiceTwo) {
+      if(choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceOne.src) {
+              return{...card,matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        
+        
+        resetTurn()
+
+      }
+      else{
+        
+        resetTurn()
+      }
+    }
+
+  },[choiceOne,choiceTwo])
+  console.log(cards)
+  
+  
+  //! Reset Choice increase turn 
+
+  const resetTurn = () => {
+    setChoiceOne()
+    setChoiceTwo()
+    setTurns(prevTurns => prevTurns + 1)
+  }
 
 
   return (
@@ -45,11 +82,11 @@ function App() {
 
       <div className='card-grid'>
         {cards.map(card => (
-        <SingleCard 
-        key={card.id} 
-        card={card} 
-        handleChoice={handleChoice}
-        />
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+          />
         ))}
 
       </div>
